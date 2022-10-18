9689c1c5a4d2629b8f51d9dbaa718dd927789660
Alice Nouhouemale <alicenouhouemale@Alices-MacBook-Pro.local>
2022-10-17 21:54:13 -0700

    update

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
   // inquirer.prompt(questions).then((answers) => {
-  //   writeToFile("README.md", generateMarkdown({ ...answers }));
+  // writeToFile("README.md", generateMarkdown({ ...answers }));
   // });
 }
 
